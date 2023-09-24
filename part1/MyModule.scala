3076e4308ba7b1ea1591b389e566ec205abe18f1
samuel wondimagegnehu <samwondim@gmail.com>
2023-09-25 00:15:57 +0300

    modified the isSorted method

--- part1/MyModule.scala
+++ part1/MyModule.scala
@@ -13,6 +13,13 @@ object MyModule {
     go(n)
   }
 
+  def fibonnaci(n: Int): Int = {
+    def loop(n: Int, prev: Int, curr: Int): Int = 
+      if(n == 0) prev
+      else loop(n - 1, curr, prev + curr)
+    loop(n, 0, 1)    
+  }
+
   def factorial(n: Int): Int = {
     def go(n: Int, acc: Int): Int =
       if(n <= 0) acc
@@ -28,22 +35,22 @@ object MyModule {
       else loop(n + 1)
     loop(0)
   }
- /* 
+  
   def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
-    @annotaion.tailrec
-    def go(n: Int): Int =
-      if(n >= as.length) -1
-      else if(ordered(as(n), as(n + 1))) n
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if(n >= as.length - 1) true
+      else if(ordered(as(n), as(n + 1))) false
       else go(n + 1)
     go(0)
   }
-*/
+
   private def formatResult(name: String, n: Int, f: Int => Int) = {
     val msg = "The %s of %d is %d"
     msg.format(name, n, f(n))
   }  
   
   def main(args: Array[String]): Unit = 
-    println(formatResult("absolute value", -100, abs))    
-    println(formatResult("factorial", 10, factorial))
+    // println(formatResult("absolute value", -100, abs))    
+    // println(formatResult("factorial", 10, factorial))
 }

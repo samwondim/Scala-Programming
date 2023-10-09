0bd17e8f8467506b1bbba383ec68c17bd6353a49
samuel wondimagegnehu <samwondim@gmail.com>
2023-10-09 13:09:30 +0300

    refactored code removing duplicate functions and syntax error

--- part1/datastructures.scala
+++ part1/datastructures.scala
@@ -16,11 +16,6 @@ object List {
     case Cons(x, xs) => x * product(xs)
   }
 
-  def tail(ints: List[Int]): List[Int] = ints match {
-    case Nil => sys.error("tail of empty list")
-    case Cons(_, t) => t
-  }
-
   def apply[A](as: A*): List[A] = //variadic function syntax
     if(as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
@@ -39,10 +34,6 @@ object List {
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Cons(h, t) if f(h) => dropWhile(t, f)
     case _ => l
-
-  @main def main(args: Array[String]): Unit = {
-    val l1: List[Int] = List(1,2,3,4)
-    val l2: List[Int] = tail(l1)
-    println(l2)  
   }
+
 }
